ba8c5e1a04c0a699694f2e27a24295fc8ab78009
sebasHeinsohn <sebasbc.96@gmail.com>
2017-10-27 17:56:07 -0500

    Capitulo 8 sesion 1
    explicacion de componentes anidados y como evitar la creacion de plantilla html para archivos pequeños de este tipo

--- appCurso/src/app/fechaactual/fechaactual.component.ts
+++ appCurso/src/app/fechaactual/fechaactual.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fechaactual',
-  templateUrl: './fechaactual.component.html',
+  template: `
+<p> Manizales, {{ hoy | date:'d/M/y H:m'}} </p>
+<app-copyright></app-copyright>
+`,
   styleUrls: ['./fechaactual.component.css']
 })
 
