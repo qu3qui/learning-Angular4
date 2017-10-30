7e1d56588e5927b7a704b36f50a86e01e4429bde
sebasHeinsohn <sebasbc.96@gmail.com>
2017-10-30 14:32:47 -0500

    Two-way Binding
    Se explica como se puede utilizar el ng-module en Angular 4, el cual difiere un poco de AngularJS

--- appCurso/src/app/app.module.ts
+++ appCurso/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FechaactualComponent } from './fechaactual/fechaactual.component';
@@ -8,6 +9,7 @@ import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
 import { EjmetodoComponent } from './ejmetodo/ejmetodo.component';
 import { EjpropertybindingComponent } from './ejpropertybinding/ejpropertybinding.component';
 import { EjeventbindingComponent } from './ejeventbinding/ejeventbinding.component';
+import { Ej2waybindingComponent } from './ej2waybinding/ej2waybinding.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +19,12 @@ import { EjeventbindingComponent } from './ejeventbinding/ejeventbinding.compone
     ViewmodeloComponent,
     EjmetodoComponent,
     EjpropertybindingComponent,
-    EjeventbindingComponent
+    EjeventbindingComponent,
+    Ej2waybindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
