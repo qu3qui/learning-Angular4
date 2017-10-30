ad21aa803174128ce667e4de8ac7e701d597ec46
sebasHeinsohn <sebasbc.96@gmail.com>
2017-10-30 12:35:59 -0500

    Interpolacion con métodos
    Se utiliza el metodo para mostrar informacion en la plantilla html

--- appCurso/src/app/app.module.ts
+++ appCurso/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FechaactualComponent } from './fechaactual/fechaactual.component';
 import { CopyrightComponent } from './copyright/copyright.component';
 import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
+import { EjmetodoComponent } from './ejmetodo/ejmetodo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FechaactualComponent,
     CopyrightComponent,
-    ViewmodeloComponent
+    ViewmodeloComponent,
+    EjmetodoComponent
   ],
   imports: [
     BrowserModule
