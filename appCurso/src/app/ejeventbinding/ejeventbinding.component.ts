eb088c254d4c2741cf9af324b263856942330a83
sebasHeinsohn <sebasbc.96@gmail.com>
2017-10-30 14:20:58 -0500

    Event Binding
    Se hace un ejemplo sobre como los eventos en HTML pueden modificar el modelo

---
+++ appCurso/src/app/ejeventbinding/ejeventbinding.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejeventbinding',
+  templateUrl: './ejeventbinding.component.html',
+  styleUrls: ['./ejeventbinding.component.css']
+})
+export class EjeventbindingComponent implements OnInit {
+
+  texto = 'Originalmente el texto se carga asi';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  modTexto(){ 
+      this.texto = 'Al pulsar el botón el texto se muestra asi';
+  }
+}
