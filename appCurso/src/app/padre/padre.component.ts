b054c7e71de99900f314e25b46166a700795771c
sebasHeinsohn <sebasbc.96@gmail.com>
2017-11-01 12:18:54 -0500

    @Output
    como pasar parametros de un metodo de un componente a otro componente

--- appCurso/src/app/padre/padre.component.ts
+++ appCurso/src/app/padre/padre.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class PadreComponent implements OnInit {
 
   avisos: string[] = ['Aviso1', 'Aviso2', 'Aviso3'];
+  texto: string;
+  mostrarMensaje(event){
+    this.texto = event + ' marcado como leido';
+  }
   constructor() { }
 
   ngOnInit() {
