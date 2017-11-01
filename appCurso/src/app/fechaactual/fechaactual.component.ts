824efeea9e4f5cff2ffb969df405841870affd46
sebasHeinsohn <sebasbc.96@gmail.com>
2017-11-01 17:02:52 -0500

    Pipe currency
    pipe para el manejo de la representación de las monedas

--- appCurso/src/app/fechaactual/fechaactual.component.ts
+++ appCurso/src/app/fechaactual/fechaactual.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-fechaactual',
   template: `
-<p>El resultado es {{resultado | number: '3.2-2'}}</p>
+<p>La cotización actual del dolar es de {{dolareuro | currency: 'EUR': true}}</p>
 `,
   styleUrls: ['./fechaactual.component.css']
 })
 
 export class FechaactualComponent {
-  resultado: number = 1.2;
+  dolareuro: number = 0.88;
 }
