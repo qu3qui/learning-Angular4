e06ce4310553221d5f5a61da475920f1d09cdb3c
sebasHeinsohn <sebasbc.96@gmail.com>
2017-11-01 17:17:00 -0500

    Pipe i18n
    Sirve para remplazar un texto dependiendo de una variable, por ejemplo de acuerdo al sexo, dar un tratamiento especifico.

--- appCurso/src/app/fechaactual/fechaactual.component.ts
+++ appCurso/src/app/fechaactual/fechaactual.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-fechaactual',
   template: `
-<p>La cotización actual del dolar es de {{dolareuro | currency: 'EUR': true}}</p>
+<p> {{sexo | i18nSelect: encabezamiento}}  {{nombre}}</p>
 `,
   styleUrls: ['./fechaactual.component.css']
 })
 
 export class FechaactualComponent {
-  dolareuro: number = 0.88;
+  nombre: string = 'Andrés';
+  sexo: string = 'hombre';
+  encabezamiento: any = {'hombre': 'Estimado', 'mujer': 'Estimada'};
 }
