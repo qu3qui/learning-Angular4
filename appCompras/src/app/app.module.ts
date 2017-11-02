814aaa9fe30c9436f49538bf12a18d33ba4b3f7a
sebasHeinsohn <sebasbc.96@gmail.com>
2017-11-02 14:39:30 -0500

    Implementación routing
    construcción del sistema de routing para la aplicación

--- appCompras/src/app/app.module.ts
+++ appCompras/src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {ProveedoresService} from './servicios/proveedores.service';
 import { ProveedoresComponent } from './proveedores/proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'proveedores', component: ProveedoresComponent},
+  {path: '**', component: InicioComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProveedoresComponent
+    ProveedoresComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
