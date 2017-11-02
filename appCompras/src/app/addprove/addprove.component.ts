a85c5605fd4f4a4b057e83fca120bf946b062b65
sebasHeinsohn <sebasbc.96@gmail.com>
2017-11-02 17:23:41 -0500

    Template Driven 2
    Se utilizan las clases para los formularios y se crea el metodo para crear un nuevo proveedor siguiendo la técnica template driven

--- appCompras/src/app/addprove/addprove.component.ts
+++ appCompras/src/app/addprove/addprove.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-addprove',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddproveComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formpro') formpro: NgForm;
+  proveedor: any;
+
+  constructor() {
+    this.proveedor = {
+      nombre: '',
+      cif: '',
+      direccion: '',
+      cp: '',
+      localidad: '',
+      provincia: '',
+      telefono: null,
+      email: '',
+      contacto: ''
+    };
+   }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.proveedor.nombre = this.formpro.value.nombre;
+    this.proveedor.cif = this.formpro.value.cif;
+    this.proveedor.direccion = this.formpro.value.direccion;
+    this.proveedor.cp = this.formpro.value.cp;
+    this.proveedor.localidad = this.formpro.value.localidad;
+    this.proveedor.provincia = this.formpro.value.provincia;
+    this.proveedor.telefono = this.formpro.value.telefono;
+    this.proveedor.email = this.formpro.value.email;
+    this.proveedor.contacto = this.formpro.value.contacto;
+    this.formpro.reset();
   }
 
 }
--- appCompras/src/app/app.module.ts
+++ appCompras/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {ProveedoresService} from './servicios/proveedores.service';
@@ -26,7 +27,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [ProveedoresService],
   bootstrap: [AppComponent]
